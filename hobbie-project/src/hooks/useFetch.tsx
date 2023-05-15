9ecce850509a3fc6ee2f3c665201183163806a62
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-05-15 16:37:52 -0400

    Created custom hook for fetching

---
+++ hobbie-project/src/hooks/useFetch.tsx
@@ -0,0 +1,31 @@
+import { HobbieAPIResponse } from "../components/Activities"
+
+import { useEffect, useState } from "react"
+
+export default function useFetch(url: string) {
+    const [response, setResponse] = useState<HobbieAPIResponse | null>(null) 
+    const [error, setError] = useState(false)
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await fetch(url);
+            if (!response.ok) {
+              throw new Error('Error during getting data');
+            }
+            const json = await response.json();
+            setResponse(json);
+            setIsLoading(false);
+          } catch (error) {
+            setError(true);
+            setIsLoading(false);
+          }
+        };
+    
+        fetchData();
+      }, [url]);
+
+      return { response, error, isLoading }
+}
+
