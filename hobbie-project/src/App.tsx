ff63713ddddc086319556afd7161cf97f5848a8a
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-05-15 16:38:32 -0400

    Started the application and created actitivites

---
+++ hobbie-project/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Activities from './components/Activities'
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className="text-red-500 text-4xl font-bold">Hobbie API</h1>
+    <Activities/>
+    </>
+  )
+}
+
+export default App
