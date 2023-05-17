e93c5523c2bff2fdf71078912c248ee89b8c0d7b
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-05-17 17:02:19 -0400

    Added main color to the custom color palette

--- hobbie-project/tailwind.config.js
+++ hobbie-project/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#00C9A7"
+      }
+    },
   },
   plugins: [],
 }
