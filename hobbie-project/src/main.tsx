6b294adcc3d7eff95f3151a5489ae1bfcf9c94da
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-05-19 22:17:15 -0400

    Added Activity Provider

--- hobbie-project/src/main.tsx
+++ hobbie-project/src/main.tsx
@@ -1,9 +1,12 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ActivityProvider } from './context/ActivitiesContext.tsx'
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-
+    
+    <ActivityProvider>
     <App />
-
+    </ActivityProvider>
 )
