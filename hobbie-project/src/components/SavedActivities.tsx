94d667b7bc1e223b8e1f30650d3f12e7a6b0097f
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-05-22 16:54:01 -0400

    Created SavedActivities

---
+++ hobbie-project/src/components/SavedActivities.tsx
@@ -0,0 +1,30 @@
+import { useActivityContext } from "../context/ActivitiesContext"
+import Activity from "./Activity";
+import { CustomActivity } from "./Activity";
+
+
+
+
+export default function SavedActivies() {
+  const { savedActivities, deleteActivity } = useActivityContext();
+
+  return (
+    <>
+      <div>
+        <h1 className="text-4xl text-gray-950 font-bold mb-8">Saved Activities</h1>
+        {savedActivities.length === 0 ? (
+          <p>No saved activities yet.</p>
+        ) : (
+          <div className="flex gap-4">
+            {savedActivities.map((activityData: CustomActivity) => (
+              <>
+                <Activity {...activityData} key={activityData.id} />
+                <button onClick={() => deleteActivity(activityData.id)}>test</button>
+              </>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
