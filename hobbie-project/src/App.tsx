5bf5b7a1a9a7a66a1be7b400eee4b3a79ba2d5bf
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-05-24 18:08:59 -0400

    Added Firebase and updated routing

--- hobbie-project/src/App.tsx
+++ hobbie-project/src/App.tsx
@@ -2,6 +2,8 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from 'react-toastify'
 import Activities from './components/Actitivites';
+import SavedActivies from './components/SavedActivities';
+import UserProfile from './components/UserProfile';
 
 function App() {
 
@@ -12,8 +14,12 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Activities/>}/>
+        <Route path="/my-activities" element={<SavedActivies/>}/>
+        <Route path="/my-profile" element={<UserProfile/>}/>
+        
 
       </Routes>
+
       <ToastContainer/>
     </BrowserRouter>
 
---
+++ hobbie-project/src/components/tempNavbar.tsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom"
+
+export default function TempNavbar(){
+    return(
+        <div className="h-8 bg-white shadow-xl">
+            <div>
+                <ul className="flex gap-4">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/my-activities">Saved Activities</Link></li>
+                    <li><Link to="/my-profile">Profile</Link></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
