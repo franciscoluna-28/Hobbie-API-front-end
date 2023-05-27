c19073f46ae31e79a90864fc1cb84174e891e506
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-05-27 12:04:32 -0400

    Created Routing and private routes

--- hobbie-project/src/App.tsx
+++ hobbie-project/src/App.tsx
@@ -1,30 +1,42 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ToastContainer } from 'react-toastify'
-import Activities from './components/Actitivites';
-import SavedActivies from './components/SavedActivities';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import Activities from './components/Actitivites'
+import SavedActivities from './components/SavedActivities';
 import UserProfile from './components/UserProfile';
+import Login from './pages/Login';
+import { useAuthContext } from './context/UserAuthContext';
+import { Outlet } from 'react-router-dom';
+
+
+
+
+
 
 function App() {
-
+  const { currentUser } = useAuthContext();
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const PrivateWrapper = ({ currentUser }: any) => {
+  return currentUser ? <Outlet /> : <Navigate to="/login" />;
+};
 
   return (
-  <>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route element={<PrivateWrapper currentUser={currentUser} />}>
+  <Route path="/find-activities" element={<Activities />} />
+</Route><Route element={<PrivateWrapper currentUser={currentUser} />}>
+  <Route path="/saved-activities" element={<SavedActivities />} />
+</Route><Route element={<PrivateWrapper currentUser={currentUser} />}>
+  <Route path="/my-profile" element={<UserProfile/>} />
+</Route>
+        </Routes>
 
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Activities/>}/>
-        <Route path="/my-activities" element={<SavedActivies/>}/>
-        <Route path="/my-profile" element={<UserProfile/>}/>
-        
-
-      </Routes>
-
-      <ToastContainer/>
-    </BrowserRouter>
-
+        <ToastContainer />
+      </BrowserRouter>
     </>
-    )}
+  );
+}
 
-
-export default App
+export default App;
