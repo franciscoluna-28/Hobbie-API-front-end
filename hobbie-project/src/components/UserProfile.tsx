9392e0793f4d1f7adf6220dff155d1e3f4b0e6a2
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-05-27 12:06:39 -0400

    Added information to the user profile page

--- hobbie-project/src/components/UserProfile.tsx
+++ hobbie-project/src/components/UserProfile.tsx
@@ -1,11 +1,19 @@
-import TempNavbar from "./tempNavbar"
+import TempNavbar from "./Navbar"
+import { useAuthContext } from "../context/UserAuthContext";
+
+
+
 
 export default function UserProfile(){
+    const { currentUser } = useAuthContext();
+    console.log(currentUser)
     return(
         <div>
                 <TempNavbar/>
             <h2>Profile</h2>
-            //TODO ADD SIGN OUT
+            <h3>Email: {currentUser?.email}</h3>
+            <h3>Email: {currentUser?.metadata.creationTime}</h3>
+            
         </div>
     )
 }
