502e77721c5ffccc7f16878f535dd50060fea53f
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-05-27 12:05:05 -0400

    Added Framer Motion div

--- hobbie-project/src/components/Actitivites.tsx
+++ hobbie-project/src/components/Actitivites.tsx
@@ -1,27 +1,41 @@
+import { motion } from "framer-motion";
 import { useActivityContext } from "../context/ActivitiesContext";
 import CategoriesDropdown from "./CategoriesDropdown";
 import FilteredActivitiesByCategory from "./FilteredActivitiesByCategory";
 import RecomendedActivities from "./RecommendedActivities";
-import TempNavbar from "./tempNavbar";
+import Sidebar from "./Sidebar";
 
 export default function Activities() {
   const { currentKeyword } = useActivityContext();
-  
+
   return (
     <>
-    <TempNavbar/>
-    <div className="p-8">
-      <h2 className="text-6xl text-accent font-bold mb-8 flex gap-4">Recommended <h1 className="flex text-main">Activities</h1></h2>
-      <CategoriesDropdown />
-      <div className="flex gap-4">
-        {currentKeyword === "" ? (
-          <RecomendedActivities />
-        ) : (
-          <FilteredActivitiesByCategory currentKeyword={currentKeyword} />
-        )}
+      <div className="grid grid-flow-col">
+        <Sidebar />
+        <motion.div
+          className="p-12"
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.3 }}
+        >
+          <h2 className="text-6xl text-accent font-bold mb-8 flex gap-4">
+            Recommended <h1 className="flex text-main">Activities</h1>
+          </h2>
+          <CategoriesDropdown />
+          <motion.div
+            className="flex gap-4 flex-wrap"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3 }}
+          >
+            {currentKeyword === "" ? (
+              <RecomendedActivities />
+            ) : (
+              <FilteredActivitiesByCategory currentKeyword={currentKeyword} />
+            )}
+          </motion.div>
+        </motion.div>
       </div>
-    </div>
     </>
   );
 }
-
