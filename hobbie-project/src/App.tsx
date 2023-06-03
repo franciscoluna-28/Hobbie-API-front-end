6b01326ccf046e56207f148172866f65c9a5fbda
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-06-03 12:46:32 -0400

    Moved the PrivateWrapper to utilities

--- hobbie-project/src/App.tsx
+++ hobbie-project/src/App.tsx
@@ -1,11 +1,11 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route  } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 import Activities from './components/Actitivites'
 import SavedActivities from './components/SavedActivities';
 import UserProfile from './components/UserProfile';
 import Login from './pages/Login';
 import { useAuthContext } from './context/UserAuthContext';
-import { Outlet } from 'react-router-dom';
+import { PrivateWrapper } from "../utils/PrivateWrapper"
 
 
 
@@ -14,10 +14,6 @@ import { Outlet } from 'react-router-dom';
 
 function App() {
   const { currentUser } = useAuthContext();
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const PrivateWrapper = ({ currentUser }: any) => {
-    return currentUser ? <Outlet /> : <Navigate to="/login" />;
-  };
 
   return (
     <>
