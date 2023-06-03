b4e9adae6dc28babd887d2e355666af6f16be484
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-06-03 12:48:49 -0400

    Create private wrapper component

---
+++ hobbie-project/utils/PrivateWrapper.tsx
@@ -0,0 +1,6 @@
+import { Navigate, Outlet } from 'react-router-dom';
+
+export function PrivateWrapper({ currentUser }: any) {
+    return currentUser ? <Outlet /> : <Navigate to="/login" />;
+  }
+  
