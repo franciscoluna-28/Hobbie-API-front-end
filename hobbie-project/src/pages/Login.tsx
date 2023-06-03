2191f9694a76db044cbd24f33c2763fcda751e89
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-06-03 12:48:00 -0400

    Created basic login screen

--- hobbie-project/src/pages/Login.tsx
+++ hobbie-project/src/pages/Login.tsx
@@ -1,70 +1,63 @@
 import { useRef, useState } from "react";
 import { login, signup } from "../../firebase/firebase";
 import { Navigate } from "react-router-dom";
-
-
-
 import { useAuthContext } from "../context/UserAuthContext";
 
 export default function Login() {
+  const { currentUser } = useAuthContext();
+  const emailRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+  const [loading, setLoading] = useState(false);
 
-    const { currentUser, handleLogout } = useAuthContext();
-    const emailRef = useRef<HTMLInputElement | null>(null);
-    const passwordRef = useRef<HTMLInputElement | null>(null);
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const [loading, setLoading] = useState<any>(false);
-    
-    async function handleLogin() {
-        try {
-          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-          await login(emailRef.current!.value, passwordRef.current!.value);
-          
-        } catch {
-          alert("Error!");
-        }
-        setLoading(false);
+  async function handleLogin() {
+    try {
+      if (emailRef.current && passwordRef.current) {
+        setLoading(true);
+        await login(emailRef.current.value, passwordRef.current.value);
       }
+    } catch {
+      alert('Error!');
+    }
+    setLoading(false);
+  }
 
-      async function handleSignup() {
-        try {
-          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-          await signup(emailRef.current!.value, passwordRef.current!.value);
-        } catch {
-          alert("Error!");
-        }
-        setLoading(false);
+  async function handleSignup() {
+    try {
+      if (emailRef.current && passwordRef.current) {
+        setLoading(true);
+        await signup(emailRef.current.value, passwordRef.current.value);
       }
+    } catch {
+      alert('Error!');
+    }
+    setLoading(false);
+  }
 
-      if (currentUser) {
-        return <Navigate to="/find-activities" replace={true} />;
-      }
-    
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
 
-    return(
-        <>
-    <div className="flex justify-center py-8">
-    <div className="flex flex-col gap-12">
-      <input ref={emailRef} placeholder="Email" />
-      <input ref={passwordRef} type="password" placeholder="Password" />
-      <div className="flex gap-4">
-      <button disabled={loading || currentUser} onClick={handleSignup}>
-        Sign up
-      </button>
-      <button disabled={loading || currentUser} onClick={handleLogin}>
-        Login
-      </button>
+  if (currentUser) {
+    return <Navigate to="/find-activities" replace={true} />;
+  }
 
-
-    <button disabled={loading || !currentUser} onClick={handleLogout}>
-      Log out
-    </button>
-    </div>
-
-    </div>
-  </div>
-  
-  </>
-    )
+  return (
+    <>
+      <div className="m-auto flex-col justify-center py-8">
+        <h1 className="text-center text-5xl font-semibold">Welcome!</h1>
+        <div className="flex justify-center py-8 w-full m-auto">
+          <form className="flex flex-col gap-12 md:w-1/2 justify-center" onSubmit={handleFormSubmit}>
+            <input className="border-2 p-2 rounded-md" ref={emailRef} placeholder="Email" />
+            <input className="border-2 p-2 rounded-md" ref={passwordRef} type="password" placeholder="Password" />
+            <button className="flex w-min" onClick={handleSignup}>Or signup here</button>
+            <button className="bg-main p-4 text-white font-semibold rounded-md" disabled={currentUser !== null } onClick={handleLogin}>
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 /*   const emailRef = useRef<HTMLInputElement | null>(null);
   const passwordRef = useRef<HTMLInputElement | null>(null);
