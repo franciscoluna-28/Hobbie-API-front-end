9328bd302e68b360444204768bdb47e4a07441b1
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-06-03 15:07:05 -0400

    updated types

--- hobbie-project/utils/PrivateWrapper.tsx
+++ hobbie-project/utils/PrivateWrapper.tsx
@@ -1,7 +1,7 @@
 import { User } from 'firebase/auth';
 import { Navigate, Outlet } from 'react-router-dom';
 
-export function PrivateWrapper(currentUser: User) {
+export function PrivateWrapper(currentUser: any) {
     return currentUser ? <Outlet /> : <Navigate to="/login" />;
   }
   
