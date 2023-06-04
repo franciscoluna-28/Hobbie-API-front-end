3abb37b38604e5ee27b6037d35e62c3d06084571
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-06-04 12:41:18 -0400

    Created Root Layout

--- hobbie-project/src/App.tsx
+++ hobbie-project/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route  } from "react-router-dom";
+import { BrowserRouter, Routes, Route, createBrowserRouter, createRoutesFromElements, RouterProvider  } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 import Activities from './components/Actitivites'
 import SavedActivities from './components/SavedActivities';
@@ -6,6 +6,8 @@ import UserProfile from './components/UserProfile';
 import Login from './pages/Login';
 import { useAuthContext } from './context/UserAuthContext';
 import { PrivateWrapper } from "../utils/PrivateWrapper"
+import RootLayout from "./layouts/RootLayout";
+
 
 
 
@@ -15,23 +17,31 @@ import { PrivateWrapper } from "../utils/PrivateWrapper"
 function App() {
   const { currentUser } = useAuthContext();
 
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route element={<PrivateWrapper currentUser={currentUser} />}>
-            <Route path="/find-activities" element={<Activities />} />
-          </Route><Route element={<PrivateWrapper currentUser={currentUser} />}>
-            <Route path="/saved-activities" element={<SavedActivities />} />
-          </Route><Route element={<PrivateWrapper currentUser={currentUser} />}>
-            <Route path="/my-profile" element={<UserProfile />} />
-          </Route>
-        </Routes>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+    <Route>
+      <Route path="/" element={<Login />} />
+      <Route element={<RootLayout/>}>
+      <Route element={<PrivateWrapper currentUser={currentUser} />}>
+        <Route path="/find-activities" element={<Activities />} />
+      </Route><Route element={<PrivateWrapper currentUser={currentUser} />}>
+        <Route path="/saved-activities" element={<SavedActivities />} />
+      </Route><Route element={<PrivateWrapper currentUser={currentUser} />}>
+        <Route path="/my-profile" element={<UserProfile />} />
+      </Route>
 
-        <ToastContainer />
-      </BrowserRouter>
-    </>
+      </Route>
+      </Route>
+      </>
+    )
+  )
+  
+
+  return (
+        <RouterProvider router={router}/>
+
+    
   );
 }
 
