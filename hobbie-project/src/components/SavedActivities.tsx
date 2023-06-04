e5d1e568752e0eee55c40dbbe3afee551871cf98
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-06-04 12:42:15 -0400

    Removed temporary navbar

--- hobbie-project/src/components/SavedActivities.tsx
+++ hobbie-project/src/components/SavedActivities.tsx
@@ -1,7 +1,6 @@
 import { useActivityContext } from "../context/ActivitiesContext"
 import Activity from "./Activity";
 import { CustomActivity } from "./Activity";
-import TempNavbar from "./Navbar";
 
 
 
@@ -12,7 +11,6 @@ export default function SavedActivies() {
   return (
     <>
       <div>
-        <TempNavbar/>
         <h1 className="text-4xl text-gray-950 font-bold mb-8">Saved Activities</h1>
         {savedActivities.length === 0 ? (
           <p>No saved activities yet.</p>
