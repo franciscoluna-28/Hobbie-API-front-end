d2eeef2eb3263a3fe3ce3d40547d2554324f5d7f
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-06-04 12:42:00 -0400

    Updated Fetching

--- hobbie-project/src/components/RecommendedActivities.tsx
+++ hobbie-project/src/components/RecommendedActivities.tsx
@@ -3,12 +3,16 @@ import { useActivityContext } from "../context/ActivitiesContext";
 import "react-toastify/dist/ReactToastify.css";
 import Activity from "./Activity";
 import { CustomActivity } from "./Activity";
+import useFetch from "../hooks/useFetch";
+import { useState, useEffect } from "react"
+
 
 export interface HobbieAPIResponse {
   data: CustomActivity[];
 }
 
 export default function RecomendedActivities() {
+
   const { isLoading, error, activities } = useActivityContext();
 
   if (isLoading) {
@@ -18,10 +22,28 @@ export default function RecomendedActivities() {
   if (error) {
     return <div>There was an error while loading...</div>;
   }
+/*   const { response, error, isLoading } = useFetch("http://localhost:3000/activity/get-a-few-activities")
+  const [ activities, setActivities] = useState<any>([])
+
+  useEffect(() => {
+      if (response) {
+        setActivities(response.data);
+        console.log(activities);
+      }
+    }, [activities, response]);
+
+
+  if (isLoading) {
+      return <div>Loading...</div>;
+    }
+
+    if (error) {
+      return <div>There was an error while loading...</div>;
+    } */
 
   return (
     <div>
-      <div className="flex gap-4">
+      <div className="lg:grid lg:grid-flow-col lg:gap-4 lg:col-span-2 flex flex-wrap">
         {activities.map((activityData: CustomActivity) => (
           <motion.div
             key={activityData.id}
