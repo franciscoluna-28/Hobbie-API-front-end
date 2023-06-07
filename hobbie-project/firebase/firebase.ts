32f9643d735ac31e54b48ec0bac0e0f1e6c53328
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-06-07 17:13:43 -0400

    Setup authentication with Google and the Firestore

--- hobbie-project/firebase/firebase.ts
+++ hobbie-project/firebase/firebase.ts
@@ -1,8 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth, createUserWithEmailAndPassword, signOut, signInWithEmailAndPassword } from "firebase/auth"
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signOut,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import { getFirestore, addDoc, collection, getDocs } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -13,25 +20,48 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Handling the authentication
 export const auth = getAuth();
 
-export function signup(email: string, password: string){
-    return createUserWithEmailAndPassword(auth, email, password);
+// Getting the firestore setup
+export const db = getFirestore();
+
+// collections
+export const colRef = collection(db, 'users');
+console.log(colRef)
+
+// get data
+getDocs(colRef).then((snapshot) => {
+  console.log(snapshot.docs);
+});
+
+export function signup(email: string, password: string) {
+  return createUserWithEmailAndPassword(auth, email, password);
 }
 
-export function logout(){
+export function logout() {
   return signOut(auth);
 }
 
-export function login(email: string, password: string){
-  return signInWithEmailAndPassword(auth, email, password)
+export function login(email: string, password: string) {
+  return signInWithEmailAndPassword(auth, email, password);
+}
 
+export function loginWithGoogle() {
+  const provider = new GoogleAuthProvider();
+  return signInWithPopup(auth, provider);
+}
+
+export function signupWithGoogle() {
+  const provider = new GoogleAuthProvider();
+  return signInWithPopup(auth, provider);
 }
 
 const analytics = getAnalytics(app);
-console.log(analytics)
+console.log(analytics);
