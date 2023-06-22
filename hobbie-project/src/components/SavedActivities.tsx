34aa55c7c1c2a77f15935e210482a45fc7234e28
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-06-21 21:42:43 -0400

    Added useFetch

--- hobbie-project/src/components/SavedActivities.tsx
+++ hobbie-project/src/components/SavedActivities.tsx
@@ -1,33 +1,52 @@
-import { useActivityContext } from "../context/ActivitiesContext"
+import { useEffect, useState } from "react";
 import Activity from "./Activity";
-import { CustomActivity } from "./Activity";
 import { motion } from "framer-motion";
+import useFetch from "../hooks/useFetch";
+import { auth } from "../../firebase/firebase";
+import { useActivityContext } from "../context/ActivitiesContext";
+
+console.log(auth.currentUser?.uid)
 
 
 
+export default function SavedActivities() {
+  const { savedActivities, setSavedActivities } = useActivityContext();
+  const { response, error, isLoading } = useFetch(
+    `http://localhost:3000/users/find-activities-by-user-uid/${auth.currentUser?.uid}`
+  );
 
-export default function SavedActivies() {
-  const { savedActivities } = useActivityContext();
+
+  useEffect(() => {
+    if (response) {
+      setSavedActivities(response);
+      console.log(savedActivities);
+    }
+  }, [savedActivities, response, setSavedActivities]);
+
 
   return (
     <>
       <div>
         <h1 className="text-4xl text-gray-950 font-bold mb-8">Saved Activities</h1>
-        {savedActivities.length === 0 ? (
+        {isLoading ? (
+          <p>Loading saved activities...</p>
+        ) : error ? (
+          <p>Error fetching saved activities.</p>
+        ) : response.length === 0 ? (
           <p>No saved activities yet.</p>
         ) : (
           <div className="grid grid-cols-2 gap-4">
-            {savedActivities.map((activityData: CustomActivity) => (
-           <motion.div
-           key={activityData.id}
-           initial={{ opacity: 0, y: 20 }}
-           animate={{ opacity: 1, y: 0 }}
-           exit={{ opacity: 0, y: -20 }}
-           transition={{ duration: 0.3 }}
-         >
-                <Activity {...activityData} key={activityData.id} />
-{/*                 <button onClick={() => deleteActivity(activityData.id)}>test</button> */}
-</motion.div>
+            {savedActivities.map((activityData: any) => (
+              <motion.div
+                key={activityData.id}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -20 }}
+                transition={{ duration: 0.3 }}
+              >
+                <Activity {...activityData} />
+                {/* <button onClick={() => deleteActivity(activityData.id)}>test</button> */}
+              </motion.div>
             ))}
           </div>
         )}
@@ -35,3 +54,4 @@ export default function SavedActivies() {
     </>
   );
 }
+
