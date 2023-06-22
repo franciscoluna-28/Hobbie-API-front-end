adecffc6f12d56f5b89f6c742837631e5a1d8eb2
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-06-21 21:41:25 -0400

    test

--- hobbie-project/src/hooks/useAuth.tsx
+++ hobbie-project/src/hooks/useAuth.tsx
@@ -2,6 +2,7 @@ import { onAuthStateChanged } from "firebase/auth";
 import { useEffect, useState } from "react";
 import { auth } from "../../firebase/firebase";
 
+
 export default function useAuth(){
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const [ currentUser, setCurretUser ] = useState<any>();
@@ -9,8 +10,11 @@ export default function useAuth(){
     useEffect(() => {
         const unsub = onAuthStateChanged(auth, user => setCurretUser(user))
         return unsub;
-    }, 
-    [])
+    },
+
+    []
+    
+    )
 
     return currentUser;
 
