207b1a8e490481a79c621927f0271f52fa9d1c37
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-06-26 18:18:26 -0400

    format code

--- hobbie-project/src/components/UserProfile.tsx
+++ hobbie-project/src/components/UserProfile.tsx
@@ -1,24 +1,16 @@
 import { useActivityContext } from "../context/ActivitiesContext";
 import { useAuthContext } from "../context/UserAuthContext";
 
+export default function UserProfile() {
+  const { currentUser } = useAuthContext();
+  const { savedActivities } = useActivityContext();
 
-
-
-export default function UserProfile(){
-    const { currentUser } = useAuthContext();
-    const { savedActivities } = useActivityContext();
-
-    return(
-        <div className="flex flex-col flex-1">
-            <h2 className="font-bold text-6xl text-left flex">User Profile</h2>
-            <h3>Email: {currentUser?.email}</h3>
-            <h3>{currentUser?.emailVerified}</h3>
-            <h3>Number of saved activities: {savedActivities.length}</h3>
-            
-        </div>
-    )
-
-
-    
+  return (
+    <div className="flex flex-col flex-1">
+      <h2 className="font-bold text-6xl text-left flex">User Profile</h2>
+      <h3>Email: {currentUser?.email}</h3>
+      <h3>{currentUser?.emailVerified}</h3>
+      <h3>Number of saved activities: {savedActivities.length}</h3>
+    </div>
+  );
 }
-
