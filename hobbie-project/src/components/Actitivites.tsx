6ed48ac790f338cc0bfdeac727d0e2b1de0733dc
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-06-26 18:17:47 -0400

    removed unused declarations

--- hobbie-project/src/components/Actitivites.tsx
+++ hobbie-project/src/components/Actitivites.tsx
@@ -3,43 +3,34 @@ import { useActivityContext } from "../context/ActivitiesContext";
 import CategoriesDropdown from "./CategoriesDropdown";
 import FilteredActivitiesByCategory from "./FilteredActivitiesByCategory";
 import RecomendedActivities from "./RecommendedActivities";
-import axios from "axios";
-import { auth } from "../../firebase/firebase";
-
-
 
 export default function Activities() {
   const { currentKeyword } = useActivityContext();
-  // Send the user UID to the backend
-
-
-
-
 
   return (
     <>
+      <motion.div
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.3 }}
+      >
+        <h1 className="lg:text-6xl text-5xl text-accent font-bold mb-8 lg:flex gap-4">
+          Recommended Activities
+        </h1>
+        <CategoriesDropdown />
         <motion.div
-          initial={{ opacity: 0, x: -100 }}
-          animate={{ opacity: 1, x: 0 }}
+          className="flex gap-4 flex-wrap"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.3 }}
-          
-          >
-          <h1 className="lg:text-6xl text-5xl text-accent font-bold mb-8 lg:flex gap-4">
-            Recommended Activities</h1>
-          <CategoriesDropdown />
-          <motion.div
-            className="flex gap-4 flex-wrap"
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.3 }}
-          >
-            {currentKeyword === "" ? (
-              <RecomendedActivities />
-            ) : (
-              <FilteredActivitiesByCategory currentKeyword={currentKeyword} />
-            )}
-          </motion.div>
+        >
+          {currentKeyword === "" ? (
+            <RecomendedActivities />
+          ) : (
+            <FilteredActivitiesByCategory currentKeyword={currentKeyword} />
+          )}
         </motion.div>
+      </motion.div>
     </>
   );
 }
