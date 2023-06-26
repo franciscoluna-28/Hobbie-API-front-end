180a857c4aee659c277fb288f7ad654564929362
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-06-26 18:17:58 -0400

    format code

--- hobbie-project/src/components/FilteredActivitiesByCategory.tsx
+++ hobbie-project/src/components/FilteredActivitiesByCategory.tsx
@@ -1,55 +1,50 @@
-import useFetch from "../hooks/useFetch"
-import { useEffect } from "react"
+import useFetch from "../hooks/useFetch";
+import { useEffect } from "react";
 import { CustomActivity } from "./Activity";
 import Activity from "./Activity";
 import { motion } from "framer-motion";
 import { useActivityContext } from "../context/ActivitiesContext";
 
-
-
-interface FilteredActivitiesByCategoryProps{
-    currentKeyword: string
+interface FilteredActivitiesByCategoryProps {
+  currentKeyword: string;
 }
 
-export default function FilteredActivitiesByCategory({currentKeyword}: FilteredActivitiesByCategoryProps){
-    const { response, error, isLoading } = useFetch(`http://localhost:3000/activity/get-activity-by-type/${currentKeyword}`)
-    const {filteredActivities, setFilteredActivities} = useActivityContext();
+export default function FilteredActivitiesByCategory({
+  currentKeyword,
+}: FilteredActivitiesByCategoryProps) {
+  const { response, error, isLoading } = useFetch(
+    `http://localhost:3000/activity/get-activity-by-type/${currentKeyword}`
+  );
+  const { filteredActivities, setFilteredActivities } = useActivityContext();
 
+  useEffect(() => {
+    if (response) {
+      setFilteredActivities(response.data);
+      console.log(filteredActivities);
+    }
+  }, [filteredActivities, response, setFilteredActivities]);
 
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
+  if (error) {
+    return <div>There was an error while loading...</div>;
+  }
 
-    
-    useEffect(() => {
-        if (response) {
-          setFilteredActivities(response.data);
-          console.log(filteredActivities);
-        }
-      }, [filteredActivities, response, setFilteredActivities]);
-
-
-    if (isLoading) {
-        return <div>Loading...</div>;
-      }
-    
-      if (error) {
-        return <div>There was an error while loading...</div>;
-      }
-
-    return(
-        <>
-
-          {filteredActivities.map((activityData: CustomActivity) => (
-                      <motion.div
-                      key={activityData.id}
-                      initial={{ opacity: 0, y: 20 }}
-                      animate={{ opacity: 1, y: 0 }}
-                      exit={{ opacity: 0, y: -20 }}
-                      transition={{ duration: 0.3 }}
-                    >
-            <Activity {...activityData} key={activityData.id} />
-            </motion.div>
-          ))}
-
-      </>
-    )
+  return (
+    <>
+      {filteredActivities.map((activityData: CustomActivity) => (
+        <motion.div
+          key={activityData.id}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -20 }}
+          transition={{ duration: 0.3 }}
+        >
+          <Activity {...activityData} key={activityData.id} />
+        </motion.div>
+      ))}
+    </>
+  );
 }
