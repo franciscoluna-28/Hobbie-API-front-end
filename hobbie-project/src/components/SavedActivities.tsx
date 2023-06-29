a5305f2bb9484a361a0d2a9efd8e9a5587e2ef94
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-06-29 18:42:37 -0400

    Changed our context to React Query

--- hobbie-project/src/components/SavedActivities.tsx
+++ hobbie-project/src/components/SavedActivities.tsx
@@ -1,16 +1,21 @@
-import { useEffect } from 'react'
 import Activity, { CustomActivity } from './Activity'
 import { motion } from 'framer-motion'
-import useFetch from '../hooks/useFetch'
-import { auth } from '../../firebase/firebase'
-import { useActivityContext } from '../context/ActivitiesContext'
+import { useQuery } from 'react-query'
+import { deleteActivityFromUser, getUserSavedActivities } from '../api/activities'
+import { useMutation } from 'react-query'
 
 export default function SavedActivities () {
-  const { savedActivities, deleteActivity } = useActivityContext()
-  const { error, isLoading } = useFetch(
-    `http://localhost:3000/users/find-activities-by-user-uid/${auth.currentUser?.uid}`
-  )
+  const { status, error, data: savedActivities } = useQuery({
+    queryKey: ["activities"],
+    queryFn: getUserSavedActivities
+  })
 
+  const deleteActivity = useMutation((id: string) => {
+    return deleteActivityFromUser(id);
+  });
+
+  if (status === "loading") return <h1>Loading...</h1>
+  if (status === "error") return <h1>{JSON.stringify(error)}</h1>
 
   return (
     <>
@@ -18,15 +23,10 @@ export default function SavedActivities () {
         <h1 className='text-4xl text-gray-950 font-bold mb-8'>
           Saved Activities
         </h1>
-        {isLoading ? (
-          <p>Loading saved activities...</p>
-        ) : error ? (
-          <p>Error fetching saved activities.</p>
-        ) : savedActivities.length === 0 ? (
-          <p>No saved activities yet.</p>
-        ) : (
-          <div className='grid grid-cols-2 gap-4'>
-            {savedActivities.map((activityData: CustomActivity) => (
+
+        <div className='grid grid-cols-2 gap-4'>
+          {savedActivities &&
+            savedActivities.data.map((activityData: CustomActivity) => (
               <motion.div
                 key={activityData.id}
                 initial={{ opacity: 0, y: 20 }}
@@ -35,12 +35,13 @@ export default function SavedActivities () {
                 transition={{ duration: 0.3 }}
               >
                 <Activity {...activityData} />
-                { <button onClick={() => deleteActivity(activityData.id)}>test</button>}
+                <button onClick={() => deleteActivity.mutate(activityData.id)}>
+                  Delete Activity
+                </button>
               </motion.div>
             ))}
-          </div>
-        )}
+        </div>
       </div>
     </>
-  )
+  );
 }
