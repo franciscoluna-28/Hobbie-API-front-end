a0dcc963205944834a737dc145870f84589ee771
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-06-29 18:41:30 -0400

    Added back auth and activity providers

--- hobbie-project/src/main.tsx
+++ hobbie-project/src/main.tsx
@@ -1,15 +1,13 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { ActivityProvider } from './context/ActivitiesContext.tsx'
-import AuthProvider from './context/UserAuthContext.tsx'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ActivityProvider } from "./context/ActivitiesContext.tsx";
+import AuthProvider from "./context/UserAuthContext.tsx";
 
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    
-    <AuthProvider>
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <AuthProvider>
     <ActivityProvider>
-    <App />
+      <App />
     </ActivityProvider>
-    </AuthProvider>
-)
+  </AuthProvider>
+);
