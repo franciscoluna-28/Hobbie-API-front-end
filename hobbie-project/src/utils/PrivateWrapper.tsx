93a10e0621e69517be8646ae8e96357bcbbce370
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-06-30 17:53:41 -0400

    Added firebase types

---
+++ hobbie-project/src/utils/PrivateWrapper.tsx
@@ -0,0 +1,12 @@
+import { User } from "@firebase/auth-types";
+import { Navigate, Outlet } from "react-router-dom";
+
+interface PrivateWrapperProps {
+  currentUser: User | null;
+}
+
+export default function PrivateWrapper(props: PrivateWrapperProps) {
+  const { currentUser } = props;
+
+  return currentUser ? <Outlet /> : <Navigate to="/login" />;
+}
