c9c199c41422dc15b4e2449b017dbd9f6450ddbf
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-06-30 17:51:41 -0400

    Deleted unused files

--- hobbie-project/utils/PrivateWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import { User } from '@firebase/auth-types';
-import { Navigate, Outlet } from 'react-router-dom';
-
-export function PrivateWrapper(currentUser: any) {
-    return currentUser ? <Outlet /> : <Navigate to="/login" />;
-  }
-
-  
-  
