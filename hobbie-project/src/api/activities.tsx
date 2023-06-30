fc47c36df5d2f5d14c67a21aeec558ad492eb4c9
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-06-30 17:52:22 -0400

    Updated API endpoints

--- hobbie-project/src/api/activities.tsx
+++ hobbie-project/src/api/activities.tsx
@@ -2,13 +2,29 @@ import axios from "axios";
 import { auth } from "../../firebase/firebase";
 import { CustomActivity } from "../components/Activity";
 
-export function getUserSavedActivities(){
-    return axios.get<CustomActivity[]>(`http://localhost:3000/users/find-activities-by-user-uid/${auth.currentUser?.uid}`)
+export function getUserSavedActivities() {
+  return axios.get<CustomActivity[]>(
+    `http://localhost:3000/users/find-activities-by-user-uid/${auth.currentUser?.uid}`
+  );
 }
 
 export function deleteActivityFromUser(activityId: string) {
-    return axios.delete(`http://localhost:3000/users/delete-activity-from-user/${auth.currentUser?.uid}`, {
+  return axios.delete<CustomActivity>(
+    `http://localhost:3000/users/delete-activity-from-user/${auth.currentUser?.uid}`,
+    {
       data: { activityId: activityId },
-    });
+    }
+  );
 }
 
+export function getNewRandomActivitiesByKeyword(query: string) {
+  return axios.get<CustomActivity[]>(
+    `https://hobbie-api-project.onrender.com/activity/get-activity-by-type/${query}`
+  );
+}
+
+export function getNewRandomActivities() {
+  return axios.get<CustomActivity[]>(
+    "https://hobbie-api-project.onrender.com/activity/get-a-few-activities"
+  );
+}
