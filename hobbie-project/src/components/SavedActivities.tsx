02c40d9d4ed611a034ad924022022cad61ecbe9e
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-07-04 16:46:08 -0400

    Updated components and layout

--- hobbie-project/src/components/SavedActivities.tsx
+++ hobbie-project/src/components/SavedActivities.tsx
@@ -1,46 +1,52 @@
-import Activity, { CustomActivity } from './Activity'
-import { motion } from 'framer-motion'
-import { useQuery } from 'react-query'
-import { deleteActivityFromUser, getUserSavedActivities } from '../api/activities'
-import { useMutation } from 'react-query'
+import Activity, { CustomActivity } from "./Activity";
+import { useQuery } from "react-query";
+import { getUserSavedActivities } from "../api/activities";
 
-export default function SavedActivities () {
-  const { status, error, data: savedActivities } = useQuery({
-    queryKey: ["activities"],
-    queryFn: getUserSavedActivities
-  })
+import ActivitiesLayout from "../layouts/ActivitiesLayout";
+import ActivityAnimation from "./Animation/ActivityAnimation";
+import useActivitiesStore from "../store/activitiesStore";
 
-  const deleteActivity = useMutation((id: string) => {
-    return deleteActivityFromUser(id);
-  });
+// The activities load only once, they'll load again only if the user wants
+export default function SavedActivities() {
+  const deleteActivity = useActivitiesStore((state) => state.deleteActivity);
+  const userSavedActivities = useActivitiesStore(
+    (state) => state.userSavedActivities
+  );
+  const setUserActivities = useActivitiesStore(
+    (state) => state.setUserSavedActivities
+  );
 
-  if (status === "loading") return <h1>Loading...</h1>
-  if (status === "error") return <h1>{JSON.stringify(error)}</h1>
+  // We get the initial activities and save them in the global state
+  const { status, error } = useQuery(
+    "savedActivities",
+    getUserSavedActivities,
+    {
+      onSuccess: (data) => {
+        setUserActivities(data.data);
+      },
+    }
+  );
+
+  if (status === "loading") return <h1>Loading...</h1>;
+  if (status === "error") return <h1>{JSON.stringify(error)}</h1>;
 
   return (
     <>
       <div>
-        <h1 className='text-4xl text-gray-950 font-bold mb-8'>
+        <h1 className="text-4xl text-gray-950 font-bold mb-8">
           Saved Activities
         </h1>
-
-        <div className='grid grid-cols-2 gap-4'>
-          {savedActivities &&
-            savedActivities.data.map((activityData: CustomActivity) => (
-              <motion.div
-                key={activityData.id}
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: -20 }}
-                transition={{ duration: 0.3 }}
-              >
-                <Activity {...activityData} />
-                <button onClick={() => deleteActivity.mutate(activityData.id)}>
+        <ActivitiesLayout>
+          {userSavedActivities &&
+            userSavedActivities.map((activityData: CustomActivity) => (
+              <ActivityAnimation activityKey={activityData.id}>
+                <Activity key={activityData.id} {...activityData} />
+                <button onClick={() => deleteActivity(activityData.id)}>
                   Delete Activity
                 </button>
-              </motion.div>
+              </ActivityAnimation>
             ))}
-        </div>
+        </ActivitiesLayout>
       </div>
     </>
   );
