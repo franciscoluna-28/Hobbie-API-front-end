6f7fc27c141cb489652c3e9d50e4ef02837a0bde
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-07-04 16:48:32 -0400

    Added token to components

--- hobbie-project/src/components/Actitivites.tsx
+++ hobbie-project/src/components/Actitivites.tsx
@@ -1,36 +1,25 @@
-import { motion } from "framer-motion";
 import { useActivityContext } from "../context/ActivitiesContext";
-import CategoriesDropdown from "./CategoriesDropdown";
 import FilteredActivitiesByCategory from "./FilteredActivitiesByCategory";
 import RecomendedActivities from "./RecommendedActivities";
+import SearchBar from "./SearchBar";
 
 export default function Activities() {
   const { currentKeyword } = useActivityContext();
 
   return (
-    <>
-      <motion.div
-        initial={{ opacity: 0, x: -100 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 0.3 }}
-      >
-        <h1 className="lg:text-6xl text-5xl text-accent font-bold mb-8 lg:flex gap-4">
-          Recommended Activities
-        </h1>
-        <CategoriesDropdown />
-        <motion.div
-          className="flex gap-4 flex-wrap"
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.3 }}
-        >
-          {currentKeyword === "" ? (
-            <RecomendedActivities />
-          ) : (
-            <FilteredActivitiesByCategory currentKeyword={currentKeyword} />
-          )}
-        </motion.div>
-      </motion.div>
-    </>
+    <div>
+      <h1 className="lg:text-6xl text-5xl text-accent font-bold mb-8 lg:flex gap-4">
+        Recommended Activities
+      </h1>
+      <SearchBar />
+      <div className="flex gap-4 flex-wrap">
+        {currentKeyword === "" ? (
+          <RecomendedActivities />
+        ) : (
+          <FilteredActivitiesByCategory token={sessionStorage.getItem("accessToken")} currentKeyword={currentKeyword} />
+        )}
+        S
+      </div>
+    </div>
   );
 }
