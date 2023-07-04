656a260f35c7b17820b4673887a3c48732d2a79e
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-07-04 16:48:46 -0400

    changed route url

--- hobbie-project/src/api/users.tsx
+++ hobbie-project/src/api/users.tsx
@@ -2,7 +2,7 @@ import { auth } from "../../firebase/firebase";
 import axios from "axios";
 
 export function createNewUser() {
-  return axios.post("http://localhost:3000/users/register-user", {
+  return axios.post("http://localhost:3000/api/user/register-user", {
     email: auth.currentUser?.email,
     uid: auth.currentUser?.uid,
   });
