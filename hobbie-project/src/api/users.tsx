31ad86e1891007dcf72972f1d278b4a7d3f2b53e
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-07-15 16:58:22 -0400

    add token to user and register it

--- hobbie-project/src/api/users.tsx
+++ hobbie-project/src/api/users.tsx
@@ -1,9 +1,22 @@
 import { auth } from "../../firebase/firebase";
 import axios from "axios";
 
+
+
 export function createNewUser() {
   return axios.post("http://localhost:3000/api/user/register-user", {
     email: auth.currentUser?.email,
     uid: auth.currentUser?.uid,
+    bearedToken: sessionStorage.getItem("accessToken")
   });
 }
+
+export function registerUserToken(uid: string) {
+  const accessToken = sessionStorage.getItem("accessToken");
+
+  return axios.post(`http://localhost:3000/api/user/register-user-token/${uid}`, {
+    bearedToken: accessToken,
+  });
+}
+
+console.log(sessionStorage.getItem("accessToken"))
