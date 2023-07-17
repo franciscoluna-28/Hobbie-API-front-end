9a5b63a7d8f0bb2dfdfee31f13304d527942d3c0
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-07-17 18:09:01 -0400

    add custom hook to get activities

--- hobbie-project/src/components/RecommendedActivities.tsx
+++ hobbie-project/src/components/RecommendedActivities.tsx
@@ -1,62 +1,60 @@
-import Activity from "./Activity";
-import { CustomActivity } from "./Activity";
+import ActivityCard from "./activityCard";
 import ActivitiesLayout from "../layouts/ActivitiesLayout";
 import ActivityAnimation from "./Animation/ActivityAnimation";
-import { useQuery } from "react-query";
-import { getNewRandomActivities } from "../api/activities";
-import { queryClient } from "../App";
 import Button from "./Button";
 import { AiOutlineReload } from "react-icons/ai";
+import { IHobbieExploreActivityWithImage } from "../types/activity";
+import { useRecommendedActivities } from "../hooks/useRecommendedActivities";
 
-export interface HobbieAPIResponse {
-  data: CustomActivity[] | any;
-
-
+interface RecomendedActivitiesProps {
+  token: string;
 }
 
-export default function RecomendedActivities() {
-  const handleUpdateClick = async () => {
-    await queryClient.invalidateQueries("activities");
+export default function RecomendedActivities({
+  token,
+}: RecomendedActivitiesProps) {
+  const { status, error, recommendedActivities, refetchRecommendedActivities } =
+    useRecommendedActivities(token);
+
+  const handleUpdateClick = () => {
+    refetchRecommendedActivities();
   };
 
-  const {
-    status,
-    error,
-    data: recommendedActivities,
-  } = useQuery({
-    queryKey: ["activities"],
-    queryFn: getNewRandomActivities,
-    retry: false,
-    staleTime: Infinity,
-  });
-
-  if (status === "loading") {
+  if (status === "loading" && !recommendedActivities) {
     return <h1>Loading...</h1>;
   }
 
   if (error) {
-    return <h1>There was en error while loading...</h1>;
+    return <h1>There was an error while loading...</h1>;
   }
 
   return (
-    <>
-      <div>
-        <Button onClick={handleUpdateClick} disabled={false}>
-          <AiOutlineReload className="text-accent text-2xl" />
-          Get New Activities
-        </Button>
-        <h1 className="text-4xl text-gray-950 font-bold mb-8">
-          Activities For You
-        </h1>
-        <ActivitiesLayout>
-          {recommendedActivities &&
-            recommendedActivities.data.map((activityData) => (
-              <ActivityAnimation activityKey={activityData.id}>
-                <Activity {...activityData} />
+    <div>
+      <Button onClick={handleUpdateClick} disabled={false}>
+        <AiOutlineReload className="text-accent text-2xl" />
+        Get New Activities
+      </Button>
+      <h1 className="text-4xl text-gray-950 font-bold mb-8">
+        Activities For You
+      </h1>
+      <ActivitiesLayout>
+        {recommendedActivities &&
+          recommendedActivities.data.map(
+            (activityData: IHobbieExploreActivityWithImage) => (
+              <ActivityAnimation
+                activityKey={activityData.activityId}
+                key={activityData.activityId}
+              >
+                <ActivityCard
+                  {...activityData}
+                  disabledFunction={() => true}
+                  isActivitySaved={true}
+                  saveActivity={() => console.log("uwu")}
+                />
               </ActivityAnimation>
-            ))}
-        </ActivitiesLayout>
-      </div>
-    </>
+            )
+          )}
+      </ActivitiesLayout>
+    </div>
   );
 }
