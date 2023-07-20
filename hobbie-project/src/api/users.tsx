e76f178ee2a34b52f3c8253e2989680e11708d6b
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-07-20 18:15:44 -0400

    add functionality to delete user

--- hobbie-project/src/api/users.tsx
+++ hobbie-project/src/api/users.tsx
@@ -1,22 +1,33 @@
+import { User } from "firebase/auth";
 import { auth } from "../../firebase/firebase";
 import axios from "axios";
-
-
 
 export function createNewUser() {
   return axios.post("http://localhost:3000/api/user/register-user", {
     email: auth.currentUser?.email,
     uid: auth.currentUser?.uid,
-    bearedToken: sessionStorage.getItem("accessToken")
+    bearedToken: sessionStorage.getItem("accessToken"),
   });
 }
 
 export function registerUserToken(uid: string) {
   const accessToken = sessionStorage.getItem("accessToken");
 
-  return axios.post(`http://localhost:3000/api/user/register-user-token/${uid}`, {
-    bearedToken: accessToken,
+  return axios.post(
+    `http://localhost:3000/api/user/register-user-token/${uid}`,
+    {
+      bearedToken: accessToken,
+    }
+  );
+}
+export function deleteUserAPI(uid: string) {
+  const accessToken = sessionStorage.getItem("accessToken");
+  return axios.delete<User>(`http://localhost:3000/api/user/delete-user/${uid}`, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
   });
 }
 
-console.log(sessionStorage.getItem("accessToken"))
+
+console.log(sessionStorage.getItem("accessToken"));
