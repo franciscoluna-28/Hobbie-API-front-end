ea2aba7bd4b0e87b1ab95386809af41c280344e0
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-07-29 21:39:57 -0400

    update recommended activities with search bar

--- hobbie-project/src/components/Actitivites.tsx
+++ hobbie-project/src/components/Actitivites.tsx
@@ -1,61 +1,45 @@
-import { useState, useEffect } from "react";
-import { auth } from "../../firebase/firebase";
-import { registerUserToken } from "../api/users";
 import { useActivityContext } from "../context/ActivitiesContext";
 import FilteredActivitiesByCategory from "./FilteredActivitiesByCategory";
 import RecomendedActivities from "./RecommendedActivities";
 import SearchBar from "./SearchBar";
-import axios from "axios";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { useSearchActivityWithQuery } from "../hooks/useSearchActivityWithQuery";
+import useSearchStore from "../store/searchStore";
+import SearchedActivities from "./activitiesSearchedByQuery";
 
 export default function Activities() {
   const { currentKeyword } = useActivityContext();
+  const { token } = useAuthContext();
+  const currentQuery = useSearchStore((state) => state.searchActivityInput);
 
-/*   if (auth.currentUser) {
-    const bearedToken = sessionStorage.getItem("accessToken");
-  
-    axios.post(`http://localhost:3000/api/user/register-user-token/${auth.currentUser.uid}`, {
-      bearedToken: bearedToken
-    })
-    .then(response => {
-      // Manejo de la respuesta exitosa del backend
-      console.log("Token registrado exitosamente:", response.data);
-      // Realiza cualquier acción adicional que necesites
-    })
-    .catch(error => {
-      // Manejo de errores de la solicitud
-      if (error.response) {
-        // El servidor respondió con un código de error
-        console.log("Error en la solicitud:", error.response.data);
-      } else if (error.request) {
-        // No se recibió una respuesta del servidor
-        console.log("No se pudo obtener respuesta del servidor:", error.request);
-      } else {
-        // Ocurrió un error durante la configuración de la solicitud
-        console.log("Error al enviar la solicitud:", error.message);
-      }
-      // Realiza cualquier acción adicional para manejar el error
-    });
-  }
-   */
+  const formattedToken = token || "";
 
-
+  const { searchActivity, obtainedActivitiesWithQuery } = useSearchActivityWithQuery(
+    formattedToken,
+    currentQuery
+  );
 
   return (
-    <div>
+    <div className="w-full min-h-full">
       <h1 className="lg:text-6xl text-5xl text-accent font-bold mb-8 lg:flex gap-4">
         Recommended Activities
       </h1>
-      <SearchBar />
-      <div className="flex gap-4 flex-wrap">
-        {currentKeyword === "" ? (
-          <RecomendedActivities token={sessionStorage.getItem("accessToken")} />
-        ) : (
-          <FilteredActivitiesByCategory
-            token={sessionStorage.getItem("accessToken")}
-            currentKeyword={currentKeyword}
-          />
-        )}
-      </div>
+      <SearchBar handleSearch={searchActivity} placeholder="Search Activities..." />
+      {currentQuery !== "" && obtainedActivitiesWithQuery ? (
+        <SearchedActivities />
+      ) : (
+        <div className="flex gap-4 flex-wrap">
+          {currentKeyword === "" ? (
+            <RecomendedActivities token={formattedToken} />
+          ) : (
+            <FilteredActivitiesByCategory
+              token={formattedToken}
+              currentKeyword={currentKeyword}
+            />
+          )}
+        </div>
+      )}
     </div>
   );
 }
+
