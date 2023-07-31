3abe573bf39be1d894dd3f683767106aeaa03687
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-07-31 16:55:45 -0400

    add notification service and methods

--- hobbie-project/src/store/activitiesStore.tsx
+++ hobbie-project/src/store/activitiesStore.tsx
@@ -1,36 +1,91 @@
 import { create } from "zustand";
-import { CustomActivity } from "../components/Activity";
 import { deleteActivityFromUser } from "../api/activities";
+import axios from "axios";
+import { IActivity } from "../types/activity";
+import NotificationService, { toastMessages } from "../utils/ToastMessages";
 
-type ActivitiesState = {
-  recommendedActivities: CustomActivity[];
-  userSavedActivities: CustomActivity[];
-  setUserSavedActivities: (activities: CustomActivity[]) => void;
-  setRecommendedActivities: (activities: CustomActivity[]) => void;
-  deleteActivity: (activityId: string) => void;
+type State = {
+  recommendedActivities: IActivity[];
+  userSavedActivities: IActivity[];
+  activitiesObtainedByQuery: IActivity[];
 };
 
-const useActivitiesStore = create<ActivitiesState>((set) => ({
+type Actions = {
+  setUserSavedActivities: (activities: IActivity[]) => void;
+  setRecommendedActivities: (activities: IActivity[]) => void;
+  setActivitiesObtainedByQuery: (activities: IActivity[]) => void;
+  deleteActivity: (activityId: string, token: string) => void;
+  saveActivity: (uid: string, token: string, id: string) => void;
+};
+
+const useActivitiesStore = create<State & Actions>((set) => ({
   recommendedActivities: [],
   userSavedActivities: [],
-  setUserSavedActivities: (activities: CustomActivity[]) =>
+  activitiesObtainedByQuery: [],
+  setUserSavedActivities: (activities: IActivity[]) =>
     set({ userSavedActivities: activities }),
-  setRecommendedActivities: (activities: CustomActivity[]) =>
+  setRecommendedActivities: (activities: IActivity[]) =>
     set({ recommendedActivities: activities }),
+  setActivitiesObtainedByQuery: (activities: IActivity[]) =>
+    set({ activitiesObtainedByQuery: activities }),
 
-  deleteActivity: async (activityId: string) => {
+  deleteActivity: async (activityId: string, token: string) => {
     try {
-      const response = await deleteActivityFromUser(activityId);
+      const response = await deleteActivityFromUser(activityId, token);
+      NotificationService.success(toastMessages.sucessOnDeletingActivity);
 
       if (response.status === 200) {
         set((state) => ({
           userSavedActivities: state.userSavedActivities.filter(
-            (activity) => activity.id !== activityId
+            (activity) => activity.activityId !== activityId
           ),
         }));
       }
     } catch (error) {
       console.error("Activity cannot be eliminated from user:", error);
+      NotificationService.error(toastMessages.successOnSavingActivity);
+    }
+  },
+
+  saveActivity: async (uid: string, token: string, id: string) => {
+    try {
+      const activityToAdd = useActivitiesStore
+        .getState()
+        .recommendedActivities.find((activity) => activity.activityId === id);
+
+      if (!activityToAdd) {
+        console.error("Activity not found");
+        return;
+      }
+
+      const response = await axios.post(
+        `http://localhost:3000/api/user/add-activity-to-user/${uid}`,
+        {
+          activityId: activityToAdd.activityId,
+          activityName: activityToAdd.activityName,
+          type: activityToAdd.type,
+          participants: activityToAdd.participants,
+          price: activityToAdd.price,
+          accessibility: activityToAdd.accessibility,
+          urls: activityToAdd.urls,
+          user: activityToAdd.user,
+          profile_image: activityToAdd.user.profile_image,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        NotificationService.success(toastMessages.successOnSavingActivity);
+        set((state) => ({
+          userSavedActivities: [...state.userSavedActivities, activityToAdd],
+        }));
+      }
+    } catch (error) {
+      console.error("Activity cannot be saved:", error);
     }
   },
 }));
