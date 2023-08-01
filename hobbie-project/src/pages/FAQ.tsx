2d291d995c85f72e124e859e7601872a05d36939
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-01 17:38:39 -0400

    add carousel to FAQ section

--- hobbie-project/src/pages/FAQ.tsx
+++ hobbie-project/src/pages/FAQ.tsx
@@ -1,57 +1,74 @@
+import { Accordion } from "flowbite-react";
+import Avatar from "../components/ui/avatar";
+import ArtistCard from "../components/ui/artistCard";
+
+
 export default function FaqPage() {
   return (
-    <section>
-      <h1 className="text-4xl text-gray-950 font-bold mb-8">
+    <section className="min-w-full">
+      <h1 className="text-4xl text-gray-950 font-bold mb-2">
         Frequently Asked Questions
       </h1>
-      <p className="text-gray-950 text-xl py-4">
-        Welcome to Hobby Explore! Below are some common questions and answers to
-        help you understand our application better.
+      <p className="text-accent/80 text-lg py-4">
+        Welcome to Hobby Explore! Find answers to common questions about our application below.
       </p>
-      <ul>
-        <li>
-          <section>
-            <h3 className="text-gray-950 text-3xl py-4 font-bold">
-              What is Hobby Explore?
-            </h3>
-            <p className="text-gray-950 text-xl py-4">
-              Hobby Explore is an application designed to help users find and
-              discover new hobbies and activities. We achieve this by leveraging
-              BoredAPI and the incredible image collection from Unsplash.
+      <Accordion>
+        {/* Sección: Qué es Hobby Explore */}
+        <Accordion.Panel>
+          <Accordion.Title>
+            What is Hobby Explore?
+          </Accordion.Title>
+          <Accordion.Content>
+            <p className="text-accent/80 text-lg py-4">
+              Hobby Explore is an application designed to help users discover new hobbies and activities using BoredAPI and captivating images from Unsplash! My idea is to make users find and discover new hobbies with a wholesome community.
             </p>
-          </section>
-        </li>
+          </Accordion.Content>
+        </Accordion.Panel>
 
-        <li>
-          <section>
-            <h3 className="text-gray-950 text-3xl py-4 font-bold">
-              Why don't some images match the activity name?
-            </h3>
-            <p className="text-gray-950 text-xl py-4">
-              That's because of the way Hobby Explore's API works. We use
-              Natural.js to process queries in the backend and then search for
-              an image based on the input. While we've coded it with precision
-              and avoided stopwords and repeated common verbs, the results may
-              sometimes be imprecise. But remember, our primary goal is to have
-              fun!
+        {/* Sección: Por qué algunas imágenes no coinciden con el nombre de la actividad */}
+        <Accordion.Panel>
+          <Accordion.Title>
+            Why don't some images match the activity name?
+          </Accordion.Title>
+          <Accordion.Content>
+            <p className="text-accent/80 text-lg py-4">
+              This happens because of the way Hobby Explore's API works. While we strive for precision, the results may sometimes be whimsical due to the creative process. And we believe imperfection is important to keep things interesting and fun!
             </p>
-          </section>
-        </li>
+          </Accordion.Content>
+        </Accordion.Panel>
 
-        <li>
-          <section>
-            <h3 className="text-gray-950 text-3xl py-4 font-bold">
-              Why are there only 3 activities available for now?
-            </h3>
-            <p className="text-gray-950 text-xl py-4">
-              Currently, we have a limited selection of activities as we focus
-              on providing the best user experience for these options. However,
-              we're actively working on expanding our activity database, and you
-              can expect more exciting activities to be added soon!
+        {/* Sección: Por qué creé este proyecto */}
+        <Accordion.Panel>
+          <Accordion.Title>
+            Why did I create this project?
+          </Accordion.Title>
+          <Accordion.Content>
+            <p className="text-accent/80 text-lg py-4">
+              I created Hobby Explore to offer a user-friendly platform for discovering new hobbies and sharing experiences with others through comments. This way, users can interact together and review activities if they're given them a try!
             </p>
-          </section>
-        </li>
-      </ul>
+          </Accordion.Content>
+        </Accordion.Panel>
+
+        {/* Sección: Cómo comenzó la idea */}
+        <Accordion.Panel>
+          <Accordion.Title>
+            How did the idea start?
+          </Accordion.Title>
+          <Accordion.Content>
+            <p className="text-accent/80 text-lg py-4">
+              Hobby Explore originated as a simple API to fetch images from Unsplash and activities from BoredAPI. It grew into a polished project by merging components from WIP projects and exploring backend development, all thanks to the encouragement of potential employers and readers like you! I really hope you're emjoying this application.
+            </p>
+          </Accordion.Content>
+        </Accordion.Panel>
+      </Accordion>
+      {/* <ArtistCard imageURL="https://yt3.googleusercontent.com/vqx6-hhQDaWcbjjJWWp5H58l8LEBxX1gStQPXJUL2fSfUyTHgpOYrZ23v7EuURuWwM7tPwJJLw=s100-c-k-c0x00ffffff-no-rj" quote="The best way to have a good idea, is to have multiple ideas and merge them together" artistName="TheFatRat"/>
+      <ArtistCard imageURL="https://yt3.googleusercontent.com/ZJGwKd4H-lsmPo6cZ2WJ7aaU6uRJYOAmj-MDIDy_Se0sUu3iM41hG3KXgVz690DeEPRqxaKx=s900-c-k-c0x00ffffff-no-rj" quote="We don't make mistakes, just happy little accidents" artistName="Bob Ross"/>
+      <ArtistCard imageURL="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg_ai4cfEBiHNGn7dCm9lYuvxQ_qIahDNNekp02W3a6yiyBXxIG1AZ2ZSGlhWxClX_zzn5Pa5cOkknqwwbJ4ok2uF5-gDLO8HuVwuhwxwWZfqyUqsJ1WEHRS5UZISbCz2_6jllB7kwfWETgkfGbaQvyJKODsVxdzQwRpkWNnJ6OEU57gzeQAf8rH6IV/s1600/Dali_Harcourt_1936.jpg" quote="Those who do not imitate anything produce nothing" artistName="Salvador Dalí"/> */}
+
+
+
+
     </section>
   );
 }
+
