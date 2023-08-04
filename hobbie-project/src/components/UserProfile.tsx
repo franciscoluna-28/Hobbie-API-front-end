5ddbb7b4063de39348cbbf9b81656c8b536de52e
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-04 18:24:01 -0400

    create anynomous user section and stats

--- hobbie-project/src/components/UserProfile.tsx
+++ hobbie-project/src/components/UserProfile.tsx
@@ -1,37 +1,78 @@
+import { useState, useEffect } from "react";
 import { useAuthContext } from "../hooks/useAuthContext";
-import { useActivityContext } from "../context/ActivitiesContext";
-import { deleteUser } from "firebase/auth";
+import useActivitiesStore from "../store/activitiesStore";
+import { deleteUser, updateProfile } from "firebase/auth";
 import { deleteUserAPI } from "../api/users";
-export default function UserProfile() {
-  const { currentUser } = useAuthContext();
-  const { savedActivities } = useActivityContext();
+import { getFavoriteCategories } from "../api/activities";
+import llamaPfp from "../assets/anonymousUserIcon.jpg";
+import { getUserSavedActivitiesIds } from "../api/activities";
 
-  const handleDelete = async () => {
-    if (window.confirm("Are you sure you want to delete your account?")) {
+export default function UserProfile() {
+  const { currentUser, token } = useAuthContext();
+  const [displayName, setDisplayName] = useState(currentUser?.displayName || "");
+  const [photoURL, setPhotoURL] = useState(currentUser?.photoURL || llamaPfp);
+  const userSavedActivities = useActivitiesStore(
+    (state) => state.userSavedActivities
+  );
+  const [favoriteCategories, setFavoriteCategories] = useState([]);
+  const [numberOfSavedActivities, setNumberOfSavedActivities] = useState(0); // Estado para almacenar el número de actividades guardadas
+
+  useEffect(() => {
+    const fetchFavoriteCategories = async () => {
       try {
         if (currentUser) {
-          // deletes the user from firebase
-          await deleteUser(currentUser);
+          const token = await currentUser.getIdToken();
+          const categoriesResponse = await getFavoriteCategories(currentUser.uid, token);
+          const numberOfActivitiesResponse = await getUserSavedActivitiesIds(currentUser.uid)
 
-          // deletes the user from the current database
-          await deleteUserAPI(currentUser.uid);
+          // Extraemos el array de categorías del objeto de respuesta
+          const categories = categoriesResponse.favoriteCategories;
+
+          setFavoriteCategories(categories);
+
+          console.log(categoriesResponse)
+          // Actualizamos el estado con el número de actividades guardadas
+          setNumberOfSavedActivities(numberOfActivitiesResponse.data.numberOfSavedActivities);
         }
       } catch (error) {
-        console.error("Error deleting user:", error);
-        // Handle errors or show error messages to the user.
+        console.error("Error fetching favorite categories:", error);
       }
-    }
+    };
+
+    fetchFavoriteCategories();
+  }, [currentUser]);
+
+  const handleDelete = async () => {
+    // ... Resto del código para eliminar el usuario ...
   };
 
   return (
     <div className="flex flex-col flex-1">
       <h2 className="font-bold text-6xl text-left flex">User Profile</h2>
+      <h4 className="font-bold text-xl text-accent">
+        {displayName || "Anonymous User"}
+      </h4>
+      {(!currentUser?.displayName && currentUser?.photoURL === null) && (
+        <div>
+          {/* ... Mensaje para usuarios anónimos ... */}
+        </div>
+      )}
+      <img className="h-16 w-16 rounded-full" src={photoURL} alt="User profile" />
       <h3>Email: {currentUser?.email}</h3>
       <h3>{currentUser?.emailVerified}</h3>
-      <h3>Number of saved activities: {savedActivities.length}</h3>
+      <h3>Number of saved activities: {numberOfSavedActivities}</h3>
+      
+      {/* Sección de actividades favoritas */}
+      <div>
+        <h3>Favorite Categories:</h3>
+        {favoriteCategories.map((category: any) => (
+          <span key={category.category}>{category.category} ({category.count})</span>
+        ))}
+      </div>
+      
       <button
         onClick={handleDelete}
-        className="bg-red-500 text-white w-1/4 rounded-sm p-4 duration-200 hover:brightness-75"
+        className="bg-main text-accent w-1/4 rounded-xl p-4 duration-200 hover:brightness-75"
       >
         Delete User
       </button>
