308ad337ce50ecb28166458b79594321c30cfdc8
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-10 14:13:30 -0400

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 
-export default function SignUp() {
+export default function SignUpho() {
   return (
     <main>
       <h1 className="text-accent font-bold text-5xl p-4 text-center">Welcome!</h1>
