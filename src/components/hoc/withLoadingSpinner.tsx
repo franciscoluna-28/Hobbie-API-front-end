d05de9c20773fdcd3158d85bad426f2c78637782
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-11 12:27:14 -0400

    disable hoc temporarily

--- src/components/hoc/withLoadingSpinner.tsx
+++ src/components/hoc/withLoadingSpinner.tsx
@@ -1,3 +1,4 @@
+/* import React from "react";
 import { useQuery } from "react-query";
 
 // Define a type for the component props that will be wrapped by the HOC
@@ -7,14 +8,16 @@ export type WithLoadingProps<T> = {
 };
 
 // Define the HOC function with generic types
-export function withLoading<T>(queryKey: string, fetchFn: () => Promise<T>) {
+export function withLoading<T>(
+  queryKey: string,
+  fetchFn: () => Promise<T>
+) {
   return function WithLoadingComponent<Props>(
     WrappedComponent: React.ComponentType<Props & WithLoadingProps<T>>
   ): React.FC<Props> {
     return function WithLoading(props: Props) {
       const { data, isLoading } = useQuery<T>(queryKey, fetchFn);
 
-      // Ensuring data isn't undefined when mounting on the component
       const wrappedProps: WithLoadingProps<T> = {
         data: data!,
         isLoading,
@@ -23,4 +26,4 @@ export function withLoading<T>(queryKey: string, fetchFn: () => Promise<T>) {
       return <WrappedComponent {...props} {...wrappedProps} />;
     };
   };
-}
+} */
