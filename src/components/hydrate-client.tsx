241f290e6ca322ece389ea71addc324efedbb843
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-11 13:58:15 -0400

    update library import from react-query to tanstack

--- src/components/hydrate-client.tsx
+++ src/components/hydrate-client.tsx
@@ -1,7 +1,9 @@
-import { Hydrate as RQHydrate, HydrateProps } from "@tanstack/react-query";
+import { Hydrate as RQHydrate, HydrateProps } from "@tanstack/react-query"; 
 
 function Hydrate(props: HydrateProps) {
   return <RQHydrate {...props} />;
 }
+
+const text = "u"
 
 export default Hydrate;
