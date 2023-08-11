3e1eeb9c0ad41ddc03a45db92c34ce85ebfb99ba
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-11 12:29:28 -0400

    remove quotes logic and animations

--- src/components/ui/landing-page-carousel/LandingPageCarousel.tsx
+++ src/components/ui/landing-page-carousel/LandingPageCarousel.tsx
@@ -1,31 +1,18 @@
-import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import { BsQuote } from "react-icons/bs";
-import LoadingSpinner from "../spinner";
-import { Quote } from "@/types/quotes";
-import { withLoading } from "@/components/hoc/withLoadingSpinner";
-import { WithLoadingProps } from "@/components/hoc/withLoadingSpinner";
-import { fetchQuotes } from "@/api/quotes";
+import { fetchQuotes } from "@/pages/api/quotes";
+import getQueryClient from "@/lib/get-query-client";
+import Quotes from "../quotes";
+import { dehydrate } from "@tanstack/query-core";
+import Hydrate from "@/components/hydrate-client";
 
-interface LandingPageCarouselProps {
+type Props = {
   image: string;
-}
+};
 
-// TODO document the code
-const LandingPageCarousel: React.FC<
-  LandingPageCarouselProps & WithLoadingProps<Quote[]>
-> = ({ image, data: quotes, isLoading }) => {
-  const [currentQuoteIndex, setCurrentQuoteIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentQuoteIndex((prevIndex) =>
-        prevIndex === quotes.length - 1 ? 0 : prevIndex + 1
-      );
-    }, 5000);
-
-    return () => clearInterval(interval);
-  }, [quotes]);
+export default async function LandingPageCarousel({ image }: Props) {
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery(["quotes"], fetchQuotes);
+  const dehydratedState = dehydrate(queryClient);
 
   return (
     <div className="group h-screen w-full max-w-lg shadow-xl relative order-2 row-auto hidden lg:block">
@@ -33,39 +20,10 @@ const LandingPageCarousel: React.FC<
         className=" absolute inset-0 bg-cover bg-center max-w-lg"
         style={{ backgroundImage: `url(${image})` }}
       >
-        <motion.div
-          className={`absolute inset-0 flex items-center justify-center text-white duration-500 group-hover:bg-black group-hover:bg-opacity-50`}
-          initial={{ opacity: 0, y: 0 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: 0 }}
-          transition={{ duration: 0.5 }}
-          key={currentQuoteIndex}
-        >
-          <AnimatePresence initial={false} mode="wait">
-            {isLoading ? (
-              <LoadingSpinner aria-label="Loading quotes for the user..." />
-            ) : (
-              <motion.div
-                className="flex flex-col gap-4 p-16 opacity-0 duration-500"
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5, delay: 0.3 }}
-              >
-                <BsQuote className="text-2xl group-hover:opacity-100 opacity-0" />
-                <p className="text-xl rounded-xl text-white opacity-0 group-hover:opacity-100 duration-200">
-                  {quotes[currentQuoteIndex]?.quote}
-                </p>
-                <p className="text-xl rounded-xl text-white opacity-0 group-hover:opacity-100 duration-200">
-                  - {quotes[currentQuoteIndex]?.author}
-                </p>
-              </motion.div>
-            )}
-          </AnimatePresence>
-        </motion.div>
+        <Hydrate state={dehydratedState}>
+          <Quotes />
+        </Hydrate>
       </div>
     </div>
   );
-};
-
-// Wrapping the component within the withLoadingHOC
-export default withLoading<Quote[]>("quotes", fetchQuotes)(LandingPageCarousel);
+}
