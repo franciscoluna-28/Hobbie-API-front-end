e6f6642f73346129fd39e2d0916c4952eb5189d7
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-11 17:11:13 -0400

    create session provider sketch

--- src/app/SessionProvider.tsx
+++ src/app/SessionProvider.tsx
@@ -3,15 +3,17 @@
 import { SessionProvider as Provider } from "next-auth/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { useState } from "react";
+import { Session } from "next-auth";
 
 type Props = {
   children: React.ReactNode;
+  session: Session | null;
 };
 
-export default function SessionProvider({ children }: Props) {
+export default function SessionProvider({ children, session }: Props) {
   const [client] = useState(new QueryClient());
   return (
-    <Provider>
+    <Provider session={session}>
       <QueryClientProvider client={client}>{children}</QueryClientProvider>
     </Provider>
   );
